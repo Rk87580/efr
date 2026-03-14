3d55fd24f0d69fea277b9ba0c9f5d674f4ffb8b9
ROHIT KALSARIYA <129936493+Rk87580@users.noreply.github.com>
2026-03-14 14:46:53 +0530

    Update run.sh

--- EFR_NO_2.7.3_install_linux64/run.sh
+++ EFR_NO_2.7.3_install_linux64/run.sh
@@ -4,10 +4,10 @@ SCRIPT=$(readlink -f "$0")
 ROOTPATH=$(dirname "$SCRIPT")
 
 # check root path permissions (read, write)
-if [ ! -r $ROOTPATH/ ] || [ ! -w $ROOTPATH/ ]
+if [ ! -r "$ROOTPATH/" ] || [ ! -w "$ROOTPATH/" ]
 then
     echo "WARNING: Insufficient permissions: $ROOTPATH"
-    echo "Run `install.sh` to reset permissions"
 fi
 
-$ROOTPATH/app/node -e "eval(require('$ROOTPATH/app/boot.node').run())" $ROOTPATH/app/EFR.zip -RootPath $ROOTPATH
+# Use Render's system Node instead of bundled node binary
+node -e "eval(require('$ROOTPATH/app/boot.node').run())" "$ROOTPATH/app/EFR.zip" -RootPath "$ROOTPATH"
